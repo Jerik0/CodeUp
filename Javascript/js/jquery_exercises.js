ad1b5d5490446bfb485b23675afa55388b4a1e04
jerik0 <jcarrico1985@live.com>
2017-04-20 14:59:59 -0500

    completed third exercise

--- Javascript/js/jquery_exercises.js
+++ Javascript/js/jquery_exercises.js
@@ -2,8 +2,12 @@
 
 $(document).ready(function(){
 
+    var contentsH1 = $('#first-header').text();
+
     $('li').css('font-size', '20px');
 
     $('li, h1, p').css('color', 'blue');
+
+    alert(contentsH1);
 
 });
